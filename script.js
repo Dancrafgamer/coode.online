0df2ba1e5f9465f43bbfcdd75271d243f203dcf1
julio <juliux_9.2@hotmail.com>
2024-05-24 00:58:11 -0600

    nothing

--- script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var alertBox = document.getElementById('alert');
-    alertBox.classList.add('show');
-
-    setTimeout(function() {
-        alertBox.classList.remove('show');
-    }, 1800);
-});
